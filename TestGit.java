5cf87651bf73527a09c6c782b7481a5313f00391
MirunaVi <42377428+MirunaVi@users.noreply.github.com>
2020-03-22 15:11:47 +0200

    Commit 1: adaugare clasa TestGit

---
+++ TestGit.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.tema2;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git ! Denumirea proiectului de licenta este Asistent Inteligent de Nutritie.");
+
+	}
+
+}
