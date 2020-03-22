f1134b95b9af4d24aaddb63043205d9f92b29654
MirunaVi <42377428+MirunaVi@users.noreply.github.com>
2020-03-22 15:15:12 +0200

    Commit 2: incarcare clasa Aliment si clasa enum TipAliment

---
+++ Aliment.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.tema2;
+
+public class Aliment {
+
+	String denumire;
+	TipAliment tipAliment;
+
+	public Aliment(String denumire,TipAliment tipAliment) {
+		super();
+		this.denumire = denumire;
+		this.tipAliment = tipAliment;
+	}
+}
